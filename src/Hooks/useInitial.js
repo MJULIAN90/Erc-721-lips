460622563127d3eb608f7c8623c5c6bf75ff7bd9
Julian <michaeljulian50@gmail.com>
2022-09-28 15:01:37 -0500

    fix without provider

--- src/Hooks/useInitial.js
+++ src/Hooks/useInitial.js
@@ -3,172 +3,156 @@ import useEthers from "./useEthers";
 import useAlerts from "./useAlerts";
 
 const useInitial = () => {
-  if (window.ethereum) {
-    const {
-      messageBalance,
-      createNtfAlert,
-      messageAlert,
-      messageAlertError,
-    } = useAlerts();
-    const [listNtfs, setListNtfs] = useState([]);
-    const [isOwner, setIsOwner] = useState(false);
-    const [feeAmount, setFeeAmount] = useState(null);
-    const [balanceWallet, setBalanceWallet] = useState(0);
+  const {
+    messageBalance,
+    createNtfAlert,
+    messageAlert,
+    messageAlertError,
+  } = useAlerts();
+  const [listNtfs, setListNtfs] = useState([]);
+  const [isOwner, setIsOwner] = useState(false);
+  const [feeAmount, setFeeAmount] = useState(null);
+  const [balanceWallet, setBalanceWallet] = useState(0);
 
-    const { contract, ethers, walletAddress, signer, utils } = useEthers();
+  const { contract, ethers, walletAddress, signer, utils } = useEthers();
 
-    // console.log(contract);
-    useEffect(() => {
-      window.ethereum.on("accountsChanged", () => {
-        window.location.reload();
-      });
-    }, []);
+  useEffect(() => {
+    window.ethereum.on("accountsChanged", () => {
+      window.location.reload();
+    });
+  }, []);
 
-    useEffect(() => {
-      getNTFs();
-      connectToRopsten();
-    }, []);
+  useEffect(() => {
+    getNTFs();
+  }, []);
 
-    const connectToRopsten = async () => {
-      await window.ethereum.request({
-        method: "wallet_switchEthereumChain",
-        params: [{ chainId: `0x${Number(3).toString(16)}` }],
-      });
-    };
+  // Get balance wallet
+  const getBalanceWallet = useMemo(async () => {
+    try {
+      let balance = (await signer.getBalance()).toString();
+      let balaceEther = ethers.utils.formatEther(balance);
 
-    // Get balance wallet
-    const getBalanceWallet = useMemo(async () => {
-      try {
-        let balance = (await signer.getBalance()).toString();
-        let balaceEther = ethers.utils.formatEther(balance);
+      setBalanceWallet(balaceEther);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, [walletAddress]);
 
-        setBalanceWallet(balaceEther);
-      } catch (error) {
-        console.log(error.message);
-      }
-    }, [walletAddress]);
+  // Get fee when we create ntf in the smart contract
+  const getFee = useMemo(async () => {
+    try {
+      const fee = await contract.fee();
+      setFeeAmount(ethers.utils.formatEther(fee));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, [walletAddress]);
 
-    // Get fee when we create ntf in the smart contract
-    const getFee = useMemo(async () => {
-      try {
-        const fee = await contract.fee();
-        setFeeAmount(ethers.utils.formatEther(fee));
-      } catch (error) {
-        console.log(error.message);
-      }
-    }, [walletAddress]);
+  // Get onwer smart contract
+  const getOwner = useMemo(async () => {
+    try {
+      const findOwner = await contract.owner();
+      if (findOwner.toUpperCase() === walletAddress.toUpperCase())
+        setIsOwner(true);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, [walletAddress, isOwner]);
 
-    // Get onwer smart contract
-    const getOwner = useMemo(async () => {
-      try {
-        const findOwner = await contract.owner();
-        if (findOwner.toUpperCase() === walletAddress.toUpperCase())
-          setIsOwner(true);
-      } catch (error) {
-        console.log(error.message);
-      }
-    }, [walletAddress, isOwner]);
-
-    // Get list
-    const getNTFs = async () => {
-      try {
-        const list = await contract.getLips();
-        let array = [];
-        list.map((ntf) => {
-          const { level, name, rarity, id, dna } = ntf;
-          return array.push({
-            level,
-            name,
-            rarity,
-            id: id.toString(),
-            dna: dna.toString(),
-            ownerNtf: ntf.owner,
-          });
+  // Get list
+  const getNTFs = async () => {
+    try {
+      const list = await contract.getLips();
+      let array = [];
+      list.map((ntf) => {
+        const { level, name, rarity, id, dna } = ntf;
+        return array.push({
+          level,
+          name,
+          rarity,
+          id: id.toString(),
+          dna: dna.toString(),
+          ownerNtf: ntf.owner,
         });
-        setListNtfs(array);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
+      });
+      setListNtfs(array);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-    // Mint new Token NFT
-    const getCreateNTF = async (_name) => {
-      try {
-        await contract.connect(signer).createRandomLip(_name, {
-          value: utils.parseEther(feeAmount),
-          gasLimit: 500000,
-        });
+  // Mint new Token NFT
+  const getCreateNTF = async (_name) => {
+    try {
+      await contract.connect(signer).createRandomLip(_name, {
+        value: utils.parseEther(feeAmount),
+        gasLimit: 500000,
+      });
 
-        setTimeout(() => {
-          getNTFs();
-        }, 10000);
-        return true;
-      } catch (error) {
-        messageAlertError("asdfsa");
-        console.log(error.message);
-        return null;
-      }
-    };
+      setTimeout(() => {
+        getNTFs();
+      }, 10000);
+      return true;
+    } catch (error) {
+      messageAlertError("asdfsa");
+      console.log(error.message);
+      return null;
+    }
+  };
 
-    // Get level to your ntfs
-    const getUpLvl = async (_id) => {
-      try {
-        await contract.levelUp(_id);
+  // Get level to your ntfs
+  const getUpLvl = async (_id) => {
+    try {
+      await contract.levelUp(_id);
 
-        messageAlert("This may take a few minutes");
+      messageAlert("This may take a few minutes");
 
-        setTimeout(() => {
-          getNTFs();
-        }, 20000);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+      setTimeout(() => {
+        getNTFs();
+      }, 20000);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    // check Smart Contract balance
-    const balanceContract = async () => {
-      try {
-        const response = await contract.moneySmartContract();
-        const balanc = ethers.utils.formatEther(response.toString());
+  // check Smart Contract balance
+  const balanceContract = async () => {
+    try {
+      const response = await contract.moneySmartContract();
+      const balanc = ethers.utils.formatEther(response.toString());
 
-        messageBalance(balanc);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
+      messageBalance(balanc);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-    // Balance withdrawal only owner
-    const withdrawalOwner = async () => {
-      try {
-        await contract.connect(signer).withdraw();
-        messageAlert("Your withdrawal is in process");
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
+  // Balance withdrawal only owner
+  const withdrawalOwner = async () => {
+    try {
+      await contract.connect(signer).withdraw();
+      messageAlert("Your withdrawal is in process");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-    return {
-      getCreateNTF,
-      getUpLvl,
-      balanceContract,
-      createNtfAlert,
-      withdrawalOwner,
-      connectToRopsten,
-      messageAlertError,
-      balanceWallet,
-      getFee,
-      getOwner,
-      walletAddress,
-      listNtfs,
-      isOwner,
-      getBalanceWallet,
-      feeAmount,
-    };
-  } else {
-    return {
-      walletAddress: "",
-    };
-  }
+  return {
+    getCreateNTF,
+    getUpLvl,
+    balanceContract,
+    createNtfAlert,
+    withdrawalOwner,
+    messageAlertError,
+    balanceWallet,
+    getFee,
+    getOwner,
+    walletAddress,
+    listNtfs,
+    isOwner,
+    getBalanceWallet,
+    feeAmount,
+  };
 };
 
 export default useInitial;
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -15,29 +15,88 @@ import { images } from "../../assets";
 import CardLips from "../CardLips";
 
 const Home = () => {
-  const {
-    walletAddress,
-    listNtfs,
-    isOwner,
-    balanceWallet,
-    feeAmount,
-    getUpLvl,
-    balanceContract,
-    getCreateNTF,
-    createNtfAlert,
-    withdrawalOwner,
-    connectToRopsten,
-    messageAlertError,
-  } = useInitial();
 
-  const handleCreate = () => {
-    if (balanceWallet > feeAmount) createNtfAlert(getCreateNTF);
-    else messageAlertError("INSUFFICIENT BALANCE");
+  const connectToRopsten = async () => {
+    await window.ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: `0x${Number(3).toString(16)}` }],
+    });
   };
+
+  const render = () => {
+    const {
+      walletAddress,
+      listNtfs,
+      isOwner,
+      balanceWallet,
+      feeAmount,
+      getUpLvl,
+      balanceContract,
+      getCreateNTF,
+      createNtfAlert,
+      withdrawalOwner,
+      messageAlertError,
+    } = useInitial();
+
+    const handleCreate = () => {
+      if (balanceWallet > feeAmount) createNtfAlert(getCreateNTF);
+      else messageAlertError("INSUFFICIENT BALANCE");
+    };
+    
+    return (
+      <Container ai={"center"} style={{ padding: "10px" }}>
+        <ContainerHeader>
+          <div style={{ width: "30%", marginLeft: 20 }}>
+            <TextTitle> ¡WELCOME NTF! </TextTitle>
+          </div>
+          <ContainerHeaderBotton>
+            <ButtonCard onClick={handleCreate}>CREATE NEW NTF</ButtonCard>
+            <ButtonCard onClick={balanceContract}>
+              SMART CONTRACT BALANCE
+            </ButtonCard>
+
+            {isOwner && (
+              <ButtonCard onClick={withdrawalOwner}>
+                WITHDRAW BALANCE
+              </ButtonCard>
+            )}
+          </ContainerHeaderBotton>
+        </ContainerHeader>
+
+        {listNtfs.length === 0 ? (
+          <ContainerTitle>
+            <TextTitleCenter>CREATE YOUR FIRST NTF</TextTitleCenter>
+          </ContainerTitle>
+        ) : (
+          <Container
+            jc={"center"}
+            fd={"row"}
+            style={{
+              flexWrap: "wrap",
+              backgroundColor: "rgb(25, 39, 61)",
+              width: "80%",
+              opacity: 0.8,
+            }}
+          >
+            {listNtfs.map((item) => {
+              return (
+                <CardLips
+                  item={item}
+                  getUpLvl={getUpLvl}
+                  key={item.id}
+                  owner={walletAddress}
+                />
+              );
+            })}
+          </Container>
+        )}
+      </Container>
+    )
+  }
 
   return (
     <Screen image={images.bg1}>
-      {walletAddress === "" ? (
+      {!window.ethereum ? (
         <Container flex={1} ai={"center"} jc={"center"}>
           <TextTitle> ¡WELCOME TO NTF GAMES!</TextTitle>
           {window.ethereum && (
@@ -50,53 +109,7 @@ const Home = () => {
           )}
         </Container>
       ) : (
-        <Container ai={"center"} style={{ padding: "10px" }}>
-          <ContainerHeader>
-            <div style={{ width: "30%", marginLeft: 20 }}>
-              <TextTitle> ¡WELCOME NTF! </TextTitle>
-            </div>
-            <ContainerHeaderBotton>
-              <ButtonCard onClick={handleCreate}>CREATE NEW NTF</ButtonCard>
-              <ButtonCard onClick={balanceContract}>
-                SMART CONTRACT BALANCE
-              </ButtonCard>
-
-              {isOwner && (
-                <ButtonCard onClick={withdrawalOwner}>
-                  WITHDRAW BALANCE
-                </ButtonCard>
-              )}
-            </ContainerHeaderBotton>
-          </ContainerHeader>
-
-          {listNtfs.length === 0 ? (
-            <ContainerTitle>
-              <TextTitleCenter>CREATE YOUR FIRST NTF</TextTitleCenter>
-            </ContainerTitle>
-          ) : (
-            <Container
-              jc={"center"}
-              fd={"row"}
-              style={{
-                flexWrap: "wrap",
-                backgroundColor: "rgb(25, 39, 61)",
-                width: "80%",
-                opacity: 0.8,
-              }}
-            >
-              {listNtfs.map((item) => {
-                return (
-                  <CardLips
-                    item={item}
-                    getUpLvl={getUpLvl}
-                    key={item.id}
-                    owner={walletAddress}
-                  />
-                );
-              })}
-            </Container>
-          )}
-        </Container>
+        render ()
       )}
     </Screen>
   );
